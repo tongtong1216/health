2fc9200d09c8aa7cf32ddf8eaf434c35033d03fc
Disten2074 <qingci2074@gmail.com>
2025-07-01 12:31:02 +0800

    根据接口变动更新

--- src/mypackage/data_managers/__init__.py
+++ src/mypackage/data_managers/__init__.py
@@ -1,3 +1,6 @@
 from .user_manager import UserManager
+from .daily_exercise_manager import DailyExerciseManager
+from .exercise_types_manager import ExerciseTypesManager
+from .profile_manager import UserProfileManager
 
-__all__ = ["UserManager"]
+__all__ = ["UserManager", "DailyExerciseManager", "ExerciseTypesManager", "UserProfileManager"]
