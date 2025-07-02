488f25cfdaad54d726d403f59c7215f3e45d8155
Disten2074 <qingci2074@gmail.com>
2025-07-02 17:06:57 +0800

    删除测试文件 test.py

--- src/mypackage/test.py
+++
@@ -1,9 +0,0 @@
-from middle.visualization import Visualization
-from datetime import datetime
-
-if __name__ == '__main__':
-    username = "test3"
-    today = datetime.today().date()
-    #result = Visualization.get_week_exercise_records(username)
-    result = Visualization.get_today_food_records(username)
-    print(result)
